1a0bb845260fcdebbda5a0fb9116ad272593692f
Luke Queenan <lukequeenan@gmail.com>
2011-09-22 15:13:43 -0700

    Fixed some errors with the getopt and included a file with the server.

--- server/main.c
+++ server/main.c
@@ -10,7 +10,10 @@ int main(int argc, char **argv);
 
 int main(int argc, char **argv)
 {
+    // Initialize port and give default option in case of no user input
     int port = DEFAULT_PORT;
+    int option = 0;
+
     // Parse command line parameters using getopt
     while ((option = getopt(argc, argv, "p:")) != -1)
     {
--- server/server.c
+++ server/server.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "server.h"
 #include "../network/network.h"
 
