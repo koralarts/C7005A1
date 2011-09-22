6370a84c3e44f803ee4fcaec75aa1761918ddd09
Luke Queenan <lukequeenan@gmail.com>
2011-09-22 09:08:32 -0700

    Filled out a header file and created an empty server function.

---
+++ server/server.c
@@ -0,0 +1,8 @@
+#include "server.h"
+#include "../network/network.h"
+
+void server(int port)
+{
+    printf("Server started with port %d\n", port);
+}
+
---
+++ server/server.h
@@ -0,0 +1,16 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#define DEF_PORT 7001
+
+// Function Prototypes
+#ifdef __cplusplus
+extern "C" {
+#endif
+void server (int port);
+#ifdef __cplusplus
+}
+#endif
+#endif
+
+
