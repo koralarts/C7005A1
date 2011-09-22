77436c73d19a7588008edd82f4c2d59fb1b5e5a6
Luke Queenan <lukequeenan@gmail.com>
2011-09-22 16:13:01 -0700

    Added a systemFatal function and an initializeServer function to set up the socket to listen.

--- server/server.c
+++ server/server.c
@@ -1,10 +1,109 @@
 #include <stdio.h>
-
+#include <sys/types.h>
+#include <stdlib.h>
 #include "server.h"
 #include "../network/network.h"
+
+/* Might not need this
+typedef union epoll_data
+{
+    void *ptr;
+    int fd;
+    _uint32_t u32;
+    _unit64_t u64;
+} epoll_data_t;
+
+struct epoll_event
+{
+    // Epoll events
+    _uint32_t events;
+    // User data
+    epoll_data_t data;
+};
+*/
+
+void initializeServer(int *listenSocket, int *port);
+static void systemFatal(const char* message);
 
 void server(int port)
 {
     printf("Server started with port %d\n", port);
+}
+
+/*
+-- FUNCTION: initializeServer
+--
+-- DATE: March 12, 2011
+--
+-- REVISIONS: September 22, 2011 - Added some extra comments about failure
+--
+-- DESIGNER: Luke Queenan
+--
+-- PROGRAMMER: Luke Queenan
+--
+-- INTERFACE: void initializeServer(int *listenSocket, int *port);
+--
+-- RETURNS: void
+--
+-- NOTES:
+-- This function sets up the required server connections, such as creating a
+-- socket, setting the socket to reuse mode, binding it to an address, and
+-- setting it to listen. If an error occurs, the function calls "systemFatal"
+-- with an error message.
+*/
+void initializeServer(int *listenSocket, int *port)
+{
+    // Create a TCP socket
+    if ((*listenSocket = tcpSocket()) == -1)
+    {
+        systemFatal("Cannot Create Socket!");
+    }
+    
+    // Allow the socket to be reused immediately after exit
+    if (setReuse(&(*listenSocket)) == -1)
+    {
+        systemFatal("Cannot Set Socket To Reuse");
+    }
+    
+    // Bind an address to the socket
+    if (bindAddress(&(*port), &(*listenSocket)) == -1)
+    {
+        systemFatal("Cannot Bind Address To Socket");
+    }
+    
+    if (makeSocketNonBlocking(&(*listenSocket)) == -1)
+    {
+        systemFatal("Cannot Set Socket To Non Blocking");
+    }
+    
+    // Set the socket to listen for connections
+    if (setListen(&(*listenSocket)) == -1)
+    {
+        systemFatal("Cannot Listen On Socket");
+    }
+}
+
+/*
+-- FUNCTION: systemFatal
+--
+-- DATE: March 12, 2011
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: Aman Abdulla
+--
+-- PROGRAMMER: Luke Queenan
+--
+-- INTERFACE: static void systemFatal(const char* message);
+--
+-- RETURNS: void
+--
+-- NOTES:
+-- This function displays an error message and shuts down the program.
+*/
+static void systemFatal(const char* message)
+{
+    perror(message);
+    exit(EXIT_FAILURE);
 }
 
