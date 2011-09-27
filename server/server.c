f69d1779666c9ff1a696f0a4cdb3de105cb22075
Luke Queenan <lukequeenan@gmail.com>
2011-09-27 14:06:50 -0700

    Removed port stuff

--- server/server.c
+++ server/server.c
@@ -23,7 +23,6 @@ void server(int port)
     int epollServer = 0;
     int numberOfReadyEvents = 0;
     int count = 0;
-    char *ports = (char*)calloc(PORT_RANGE, sizeof(char));
     struct epoll_event event;
     struct epoll_event *events;
     
@@ -73,7 +72,7 @@ void server(int port)
                 // We have a connection waiting on the listening socket
                 while (1)
                 {
-                    /*
+                    
                     if ((socket = acceptConnection(&listenSocket)) == -1)
                     {
                         if ((errno == EAGAIN) || (errno == EWOULDBLOCK))
@@ -88,12 +87,6 @@ void server(int port)
                             break;
                         }
                     }
-                    */
-                    // TODO: FORK HERE?
-                    
-                    // Create a mini server to deal with the client
-                    socket = getFreePort(&(*ports), port);
-                    initializeServer(&socket, socket);
                     
                     // Make the socket non blocking
                     if (makeSocketNonBlocking(&socket) == -1)
@@ -116,31 +109,12 @@ void server(int port)
             }
             else
             {
-                // We have a file transfer waiting to happen, spawn a process
+                
             }
         }
     }
     
     printf("Server Closing!\n");
-}
-
-int getFreePort(char *buffer, int startingPort)
-{
-    int count = 0;
-    for (count = 0; count < PORT_RANGE; count++)
-    {
-        if (buffer[count] == 0)
-        {
-            buffer[count] = 1;
-            return startingPort + count;
-        }
-    }
-    return -1;
-}
-
-void setFreePort(char *buffer, int startingPort, int port)
-{
-    buffer[port - startingPort] = 0;
 }
 
 void processConnection(int socket)
